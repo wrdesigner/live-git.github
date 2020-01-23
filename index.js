582239310251eda46661677b83f217fa98a2eab4
wcostalonga86@gmail.com <wcostalonga86@gmail.com>
2020-01-23 18:41:56 +0000

    chore: Altera retorno da rota

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express()
 
 app.get('/teste', (req, res) =>{
-  return res.jason({hello: 'world'})
+  return res.jason({hello: 'world2'})
 });
 
 app.listen(3333);
